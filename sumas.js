2032c6a0b2b940a65234b87092c1daf22b89d44f
Gladius4110 <92822117+Gladius4110@users.noreply.github.com>
2021-11-23 14:39:56 -0500

    Add files via upload

---
+++ sumas.js
@@ -0,0 +1,17 @@
+number1=prompt("Diga un número")
+number2=prompt("Diga otro número diferente")
+suma=parseInt(number1)+parseInt(number2)
+resta=number1-number2
+multiplicacion=number1*number2
+division=number1/number2
+var n =
+Number(number1) + "<br>" +
+Number(number2) + "<br>" +
+Number(suma) + "<br>" +
+Number(resta) + "<br>" +
+Number(multiplicacion) + "<br>" +
+Number(division) + "<br>" 
+console.log ("La suma de los dos números es "+suma)
+console.log ("La resta es "+resta)
+console.log ("La multiplicación es "+multiplicacion)
+console.log ("Y la división es "+division)
